cbde78533872b434ff3f3b844368175c70db7e6c
Arjun Thumbayil <arjunthumbayil@gmail.com>
2022-12-05 05:50:14 +0530

    setup headers

--- src/examples/2-headers.js
+++ src/examples/2-headers.js
@@ -1,22 +1,28 @@
-import { useState } from 'react';
-import axios from 'axios';
+import { useState } from "react";
+import axios from "axios";
 
-const url = 'https://icanhazdadjoke.com/';
+const url = "https://icanhazdadjoke.com/";
 // Accept : 'application/json'
 
 const Headers = () => {
-  const [joke, setJoke] = useState('random dad joke');
+  const [joke, setJoke] = useState("random dad joke");
 
   const fetchDadJoke = async () => {
-    console.log('fetch dad joke');
+    const { data } = await axios(url, {
+      headers: {
+        Accept: "application/json",
+      },
+    });
+    console.log(data.joke);
+    setJoke(data.joke);
   };
 
   return (
-    <section className='section text-center'>
-      <button className='btn' onClick={fetchDadJoke}>
+    <section className="section text-center">
+      <button className="btn" onClick={fetchDadJoke}>
         random joke
       </button>
-      <p className='dad-joke'>{joke}</p>
+      <p className="dad-joke">{joke}</p>
     </section>
   );
 };
